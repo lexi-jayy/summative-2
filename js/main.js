71a95aa3789303c0c5276057f139a6383dd566a4
Lexi-Jayy Critchley <845869864@bun2-206-18.local>
2018-12-13 15:05:57 +1300

    this is my second summative

---
+++ js/main.js
@@ -0,0 +1,557 @@
+ var accomodation = [
+	{
+		accomodationname: "Brentwood Hotel",
+		accomodationtype: "Hotel",
+		people: "1 - 2 people",
+		nights: "min 1 night, max 5 nights",
+		price: 175,
+		image: "brentwoodHotel.jpg",
+		text: "View Details"
+
+	 
+	},
+	{
+		accomodationname: "Copthorne",
+		accomodationtype: "Hotel",
+		people: "1 - 2 people",
+		nights: "min 1 night, max 5 nights",
+		price: 175,
+		image: "copthorne.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Grand Mercure",
+		accomodationtype: "Hotel",
+		people: "1 - 2 people",
+		nights: "min 1 night, max 5 nights",
+		price: 175,
+		image: "grandMercure.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "InterContinetal",
+		accomodationtype: "Hotel",
+		people: "1 - 2 people",
+		nights: "min 1 night, max 5 nights",
+		price: 175,
+		image: "intercontinental-wellington.jpeg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "James Cook Hotel",
+		accomodationtype: "Hotel",
+		people: "1 - 2 people",
+		nights: "min 1 night, max 5 nights",
+		price: 175,
+		image: "JamesCookHotel.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "West Plaza",
+		accomodationtype: "Hotel",
+		people: "1 - 2 people",
+		nights: "min 1 night, max 5 nights",
+		price: 175,
+		image: "westPlaza.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: " QT Wellington",
+		accomodationtype: "Hotel",
+		people: "1 - 2 people",
+		nights: "min 1 night, max 5 nights",
+		price: 175,
+		image: "qtWellington.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Wellington",
+		accomodationtype: "Hotel",
+		people: "1 - 2 people",
+		nights: "min 1 night, max 5 nights",
+		price: 175,
+		image: "wellington.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Thorndon Hotel",
+		accomodationtype: "Hotel",
+		people: "1 - 2 people",
+		nights: "min 1 night, max 5 nights",
+		price: 175,
+		image: "thorndonHotel.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Rydges",
+		accomodationtype: "Hotel",
+		people: "1 - 2 people",
+		nights: "min 1 night, max 5 nights",
+		price: 175,
+		image: "rydges.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "West Plaza",
+		accomodationtype: "Hotel",
+		people: "1 - 2 people",
+		nights: "min 1 night, max 5 nights",
+		price: 175,
+		image: "westPlaza.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Hotel Waterloo & Backpackers",
+		accomodationtype: "Hostel",
+		people: "1 person",
+		nights: "min 1 night, max 10 nights",
+		price: 30,
+		image: "hotel-waterloo-backpackers.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Base Backpackers",
+		accomodationtype: "Hostel",
+		people: "1 person",
+		nights: "min 1 night, max 10 nights",
+		price: 30,
+		image: "baseBackpackers.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Cambridge Hotel",
+		accomodationtype: "Hostel",
+		people: "1 person",
+		nights: "min 1 night, max 10 nights",
+		price: 30,
+		image: "cambridgeHotel.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "The Cambridge",
+		accomodationtype: "Hostel",
+		people: "1 person",
+		nights: "min 1 night, max 10 nights",
+		price: 30,
+		image: "TheCambridge.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Laneway",
+		accomodationtype: "Hostel",
+		people: "1 person",
+		nights: "min 1 night, max 10 nights",
+		price: 30,
+		image: "laneway.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "lodge In The City",
+		accomodationtype: "Hostel",
+		people: "1 person",
+		nights: "min 1 night, max 10 nights",
+		price: 30,
+		image: "lodgeInTheCity.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Nomads Capital",
+		accomodationtype: "Hostel",
+		people: "1 person",
+		nights: "min 1 night, max 10 nights",
+		price: 30,
+		image: "nomadsCapital.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "The Dewlling",
+		accomodationtype: "Hostel",
+		people: "1 person",
+		nights: "min 1 night, max 10 nights",
+		price: 30,
+		image: "theDewlling.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "The Setup",
+		accomodationtype: "Hostel",
+		people: "1 person",
+		nights: "min 1 night, max 10 nights",
+		price: 30,
+		image: "theSetup.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Trek Global",
+		accomodationtype: "Hostel",
+		people: "1 person",
+		nights: "min 1 night, max 10 nights",
+		price: 30,
+		image: "trekGlobal.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Yha Wellington",
+		accomodationtype: "Hostel",
+		people: "1 person",
+		nights: "min 1 night, max 10 nights",
+		price: 30,
+		image: "yhaWellington.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Acorns",
+		accomodationtype: "House",
+		people: "1 - 4 people",
+		nights: "min 2 nights, max 15 nights",
+		price: 240,
+		image: "acorns.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Austin Villa",
+		accomodationtype: "House",
+		people: "1 - 4 people",
+		nights: "min 2 nights, max 15 nights",
+		price: 240,
+		image: "austinvilla.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "BeachFront Bed",
+		accomodationtype: "House",
+		people: "1 - 4 people",
+		nights: "min 2 nights, max 15 nights",
+		price: 240,
+		image: "beachfrontBed.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Bidwill Bnb",
+		accomodationtype: "House",
+		people: "1 - 4 people",
+		nights: "min 2 nights, max 15 nights",
+		price: 240,
+		image: "bidwillBnb.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Booklovers",
+		accomodationtype: "House",
+		people: "1 - 4 people",
+		nights: "min 2 nights, max 15 nights",
+		price: 240,
+		image: "booklovers.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Brentwood Hotel",
+		accomodationtype: "House",
+		people: "1 - 4 people",
+		nights: "min 2 nights, max 15 nights",
+		price: 240,
+		image: "brentwoodHotel.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Edgewater Lodge",
+		accomodationtype: "House",
+		people: "1 - 4 people",
+		nights: "min 2 nights, max 15 nights",
+		price: 240,
+		image: "edgewaterLodge.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Millies Bnb",
+		accomodationtype: "House",
+		people: "1 - 4 people",
+		nights: "min 2 nights, max 15 nights",
+		price: 240,
+		image: "milliesBnb.jpeg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Richmond Guest",
+		accomodationtype: "House",
+		people: "1 - 4 people",
+		nights: "min 2 nights, max 15 nights",
+		price: 240,
+		image: "richmondGuest.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Laneway",
+		accomodationtype: "House",
+		people: "1 - 4 people",
+		nights: "min 2 nights, max 15 nights",
+		price: 240,
+		image: "laneway.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Wellington Bnb",
+		accomodationtype: "House",
+		people: "1 - 4 people",
+		nights: "min 2 nights, max 15 nights",
+		price: 240,
+		image: "wellingtonBnb.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "747 Motel",
+		accomodationtype: "Motel",
+		people: "2 - 4 people",
+		nights: "min 3 nights, max 10 nights",
+		price: 90,
+		image: "747Motel.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "88 Wallace st",
+		accomodationtype: "Motel",
+		people: "2 - 4 people",
+		nights: "min 3 nights, max 10 nights",
+		price: 90,
+		image: "88Wallace.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Adelaide Motel",
+		accomodationtype: "Motel",
+		people: "2 - 4 people",
+		nights: "min 3 nights, max 10 nights",
+		price: 90,
+		image: "adelaideMotel.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Airport",
+		accomodationtype: "Motel",
+		people: "2 - 4 people",
+		nights: "min 3 nights, max 10 nights",
+		price: 90,
+		image: "airport.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Airport Motel",
+		accomodationtype: "Motel",
+		people: "2 - 4 people",
+		nights: "min 3 nights, max 10 nights",
+		price: 90,
+		image: "airportMotel.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Capital Gateway",
+		accomodationtype: "Motel",
+		people: "2 - 4 people",
+		nights: "min 3 nights, max 10 nights",
+		price: 90,
+		image: "capitalGateway.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Apollo Lodge",
+		accomodationtype: "Motel",
+		people: "2 - 4 people",
+		nights: "min 3 nights, max 10 nights",
+		price: 90,
+		image: "apolloLodge.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Ascot",
+		accomodationtype: "Motel",
+		people: "2 - 4 people",
+		nights: "min 3 nights, max 10 nights",
+		price: 90,
+		image: "ascot.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Belle Vista",
+		accomodationtype: "Motel",
+		people: "2 - 4 people",
+		nights: "min 3 nights, max 10 nights",
+		price: 90,
+		image: "belleVista.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Capital View",
+		accomodationtype: "Motel",
+		people: "2 - 4 people",
+		nights: "min 3 nights, max 10 nights",
+		price: 90,
+		image: "capitalView.jpg",
+		text: "View Details"
+	 
+	},
+	{
+		accomodationname: "Victoria Court",
+		accomodationtype: "Motel",
+		people: "2 - 4 people",
+		nights: "min 3 nights, max 10 nights",
+		price: 90,
+		image: "victoriaCourt.jpg",
+		text: "View Details"
+	 
+	}
+]
+
+
+$(document).ready(function () {
+
+	$("#showFilter").click(function() {
+		$("#slide").show( "slide", {direction: "right" } );
+		$("#filterModal").show();
+	});
+
+	$("#accBtn").click(function() {
+		$("#slide").hide( "slide", {direction: "right" } );
+		$("#filterModal").hide(); 
+	});
+	$("#closebtn").click(function() {
+		$("#slide").hide( "slide", {direction: "right" } );
+		$("#filterModal").hide(); 
+	});
+});
+
+var productsContainer = document.getElementById('allProducts');
+
+for (var i = 0; i < accomodation.length; i++) {
+    addProductToPage(accomodation[i]);
+}
+var totalProducts = document.getElementById('totalProducts'); 
+totalProducts.innerHTML = accomodation.length + " items found";
+
+var type,price,total;
+
+function filterResults() { 
+	var i;
+	type = document.getElementById('hotel').value;
+    productsContainer.innerHTML = '';
+
+for (var i = 0; i < accomodation.length; i++) {
+    if (type === accomodation[i].accomodationtype) {
+			addProductToPage(accomodation[i]);
+			// $("#view").click(function(){
+			// 	if (type=="Hostel"){
+			// 		$("#1").show();
+			// 		$("#2").hide();
+			// 		$("#3").hide();
+			// 		$("#4").hide();
+			// 	}
+			// });
+			
+		}
+	}
+
+}
+
+function calculate() {
+    var nights = document.getElementById('nights').value;
+    var people = document.getElementById('people').value;
+    var meals = document.getElementById('meals').value;
+    console.log(nights,people,meals);
+    if ((meals == 1) || (meals == 2) || (meals == 3)) {
+		var mealprice = 12.50;
+	} else if (meals == 4) {
+		var mealprice = 37.50;
+	} else if (meals == 5){
+		var mealprice = 0;
+
+	}
+
+	total = parseInt(nights) + mealprice;
+	console.log(total);
+	document.getElementById('totalPrice').innerHTML += total;
+}
+
+
+
+
+// Get the button that opens the modal
+var btn = document.getElementById('view');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = 'block';
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = 'none';
+}
+
+
+function addProductToPage(singleProduct){
+   var productDiv = document.createElement("div");
+    productDiv.className = "col-6 col-md-4 col__card card";
+
+    productDiv.innerHTML += "<h2 class='card__accomodationtype'>" + singleProduct.accomodationname + "</h2>";
+
+    productDiv.innerHTML += "<img class='card__img' src='images/" + singleProduct.image + "'>";
+    productDiv.innerHTML += "<p class='card__price'> $ " + singleProduct.price + "</p>";
+    productDiv.innerHTML += "<p class='card__people'>" + singleProduct.people + "</p>";
+    productDiv.innerHTML += "<p class='card__nights'>" + singleProduct.nights + "</p>";
+    productDiv.innerHTML += "<div class='btn btn-lg' id='view' data-toggle='modal' data-target='#modal'>" + singleProduct.text + "</div>";
+   
+    productDiv.innerHTML += '</div>';
+
+   productsContainer.appendChild(productDiv);
+}
+
+
+
+
+
+
+
+
